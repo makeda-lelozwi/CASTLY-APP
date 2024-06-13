467d47e21d4c79588ede7aa5cb812811dc7e81cd
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-06-13 17:19:25 +0200

    adding podcast list to explore page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import "./styles/App.css";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-// import { Routes, Route } from "react-router-dom";
-// import SignUp from "./components/SignUp";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-
-    <div>
-      <Layout/>
-    </div>
-    // <Routes>
-    //   <Route path="/" element={<SignUp />} >
-    //     <Route path="" >
-          
-    //     </Route>
-    //   </Route>
-
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/explore" element={<Layout />} >
+        
+      </Route>
+    </Routes>
   );
 }
 
