0122e4d5fd1026b8470f0f79b9399fe314510bd9
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-06-13 17:45:53 +0200

    fetched and styled podcast titles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import SignUp from "./components/SignUp";
+import AllPodcasts from "./pages/explore/AllPodcasts";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/explore" element={<Layout />} >
-        
+        <Route index element={<AllPodcasts />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/explore/AllPodcasts.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AllPodcasts = () => {
+  const [podcasts, setPodcasts] = useState([]);
+  useEffect(() => {
+    fetch("https:/podcast-api.netlify.app")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPodcasts(data);
+      });
+  }, []);
+
+  return (
+    <div className="podcasts-container">
+      {podcasts.map((podcast) => {
+        return (
+          <Link to={`${podcast.id}`} key={podcast.id}>
+            <div key={podcast.id} className="podcast-card">
+              <div className="podcast-image-container">
+                <img
+                  className="podcast-image"
+                  src={podcast.image}
+                  alt={podcast.title}
+                />
+              </div>
+              <h3 className="podcast-title">{podcast.title}</h3>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AllPodcasts;
